3336bd7c8e72f2c1699d83239dfb270de3c3f5a9
Adam Rubin <arubin@carnegierobotics.com>
2022-03-02 10:03:04 -0500

    Fixing update.

--- src/greenhouse/utils.go
+++ src/greenhouse/utils.go
@@ -7,17 +7,17 @@ import (
 )
 
 type RespObj struct {
-  Id int `json:"id"`
+	Id int `json:"id"`
 }
 
 func Create(c *Client, itemType string, item interface{}) (int, error) {
-  var respObj RespObj
+	var respObj RespObj
 	jsonBody, err := json.Marshal(item)
 	if err != nil {
 		return respObj.Id, err
 	}
 	resp, err := c.Client.R().SetBody(jsonBody).Post(fmt.Sprintf("v1/%s", itemType))
-  err = json.Unmarshal(resp.Body(), &respObj)
+	err = json.Unmarshal(resp.Body(), &respObj)
 	if err != nil {
 		return respObj.Id, err
 	}
@@ -64,6 +64,12 @@ func Update(c *Client, itemType string, id int, item interface{}) error {
 	if err != nil {
 		return err
 	}
-	_, err = c.Client.R().SetBody(jsonBody).Patch(fmt.Sprintf("v1/%s/%d", itemType, id))
+	resp, err := c.Client.R().SetBody(jsonBody).Patch(fmt.Sprintf("v1/%s/%d", itemType, id))
+	if err != nil {
+		return err
+	}
+	if !resp.IsSuccess() {
+		return errors.New(resp.Status())
+	}
 	return nil
 }
